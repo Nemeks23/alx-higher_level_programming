dc0aad0f872a6aba3d90c2c3cde5dc44fa5d76ee
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-09-22 23:05:29 +0100

    scf

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the sum, difference, product, quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
-
-    a = 10
-    b = 5
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
